1fd0c5bf54bd1aeb4d0abfbbc63e04ee96ed9e10
Chris Queen <chrisqueen10@gmail.com>
2020-05-27 19:33:17 -0400

    Added logic to fetch restaurant data from the Yelp Fusion API in gatsby-node. Once fetched, gatsby nodes are created from the response data

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,7 @@
+const fetch = require("node-fetch")
+const { YELP_API_KEY } = require("./secrets")
+
+const yelpEndpoint = "https://api.yelp.com/v3/businesses/search"
 /**
  * Implement Gatsby's Node APIs in this file.
  *
@@ -5,3 +9,50 @@
  */
 
 // You can delete this file if you're not using it
+
+const fetchFood = async params => {
+  const url = new URL(yelpEndpoint)
+  Object.entries(params).forEach(([key, value]) =>
+    url.searchParams.append(key, value)
+  )
+
+  try {
+    const res = await fetch(url.href, {
+      headers: {
+        Authorization: `Bearer ${YELP_API_KEY}`,
+      },
+    })
+    // TODO check status code
+    const food = await res.json()
+    return food
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+exports.sourceNodes = async ({
+  actions,
+  createNodeId,
+  createContentDigest,
+}) => {
+  // TODO Add breakfast, lunch, dinner options using "open_at" param
+
+  const { createNode } = actions
+  const params = {
+    location: "30318",
+    limit: 20,
+    term: "lunch",
+  }
+
+  const food = await fetchFood(params)
+
+  const nodeMeta = {
+    id: createNodeId(`food-${food.id}`),
+    internal: {
+      type: "food",
+      contentDigest: createContentDigest(food),
+    },
+  }
+  const node = { ...food, ...nodeMeta }
+  createNode(node)
+}
