9fff17422911162170f2816f2e3871bbe2e9cbeb
Chris Queen <chrisqueen10@gmail.com>
2020-05-28 13:01:48 -0400

    Added breakfast, lunch, and dinner queries

--- gatsby-node.js
+++ gatsby-node.js
@@ -30,6 +30,31 @@ const fetchFood = async params => {
   }
 }
 
+const addGatsbyNode = async (
+  createNode,
+  createNodeId,
+  createContentDigest,
+  foodType,
+  foodOptions
+) => {
+  if (foodOptions.businesses.length > 0) {
+    foodOptions.businesses.forEach(option => {
+      const nodeMeta = {
+        id: createNodeId(`food-option-${option.id}`),
+        foodType,
+        internal: {
+          type: "FoodOption",
+          contentDigest: createContentDigest(option),
+        },
+      }
+      const node = { ...option, ...nodeMeta }
+      createNode(node)
+    })
+  } else {
+    console.error("No food options were provided")
+  }
+}
+
 exports.createSchemaCustomization = ({ actions }) => {
   const { createTypes } = actions
   const typeDefs = `
@@ -40,38 +65,99 @@ exports.createSchemaCustomization = ({ actions }) => {
       price: String
       image_url: String
       url: String
+      foodType: String
     }
   `
 
   createTypes(typeDefs)
 }
 
+// Breakfast (7AM EST, Thursday, May 28, 2020) ====> 1590663600
+// Lunch (12PM EST, Thursday, May 28, 2020) ====> 1590681600
+// Dinner (5PM EST, Thursday, May 28, 2020) ====> 1590699600
+// "type" could be retrieved from key, but setting the value explicitly adds flexibility in the future
+
+const foodTypeData = (() => {
+  const breakfastTime = "1590663600"
+  const lunchTime = "1590681600"
+  const dinnerTime = "1590699600"
+  return {
+    breakfast: {
+      open_at: breakfastTime,
+      params: {
+        term: "breakfast",
+        open_at: breakfastTime,
+      },
+    },
+    lunch: {
+      open_at: lunchTime,
+      params: {
+        term: "lunch",
+        open_at: lunchTime,
+      },
+    },
+    dinner: {
+      open_at: dinnerTime,
+      params: {
+        term: "dinner",
+        open_at: dinnerTime,
+      },
+    },
+  }
+})()
+
 exports.sourceNodes = async ({
   actions,
   createNodeId,
   createContentDigest,
 }) => {
-  // TODO Add breakfast, lunch, dinner options using "open_at" param
-
   const { createNode } = actions
-  const params = {
+  const baseParams = {
     location: "30318",
     limit: 20,
-    term: "lunch",
   }
 
-  const food = await fetchFood(params)
-  if (food.businesses.length > 0) {
-    food.businesses.forEach(foodOption => {
-      const nodeMeta = {
-        id: createNodeId(`food-option-${foodOption.id}`),
-        internal: {
-          type: "FoodOption",
-          contentDigest: createContentDigest(foodOption),
-        },
-      }
-      const node = { ...foodOption, ...nodeMeta }
-      createNode(node)
-    })
-  }
+  // TODO Add breakfast, lunch, dinner options using "open_at" param
+  const { breakfast, lunch, dinner } = foodTypeData
+
+  // Replace all of the operations below with a loop through the foodTypeData object
+
+  // Fetch Breakfast Options
+  const breakfastOptions = await fetchFood({
+    ...baseParams,
+    ...breakfast.params,
+  })
+  addGatsbyNode(
+    createNode,
+    createNodeId,
+    createContentDigest,
+    "breakfast",
+    breakfastOptions
+  )
+
+  // Fetch Lunch Options
+  const lunchOptions = await fetchFood({
+    ...baseParams,
+    ...lunch.params,
+  })
+  addGatsbyNode(
+    createNode,
+    createNodeId,
+    createContentDigest,
+    "lunch",
+    lunchOptions
+  )
+
+  // Fetch Dinner Options
+  const dinnerOptions = await fetchFood({
+    ...baseParams,
+    ...dinner.params,
+  })
+  addGatsbyNode(
+    createNode,
+    createNodeId,
+    createContentDigest,
+    "dinner",
+    dinnerOptions
+  )
 }
