0eb0e8f0a1c39c1aa9496ca9af2b4a00c97438e9
Chris Queen <chrisqueen10@gmail.com>
2020-06-19 11:02:55 -0400

    Added easing function to line movement

--- src/lib/animation/food.js
+++ src/lib/animation/food.js
@@ -1,5 +1,6 @@
 import { lerp } from "canvas-sketch-util/math"
 import random from "canvas-sketch-util/random"
+import BezierEasing from "bezier-easing"
 
 /* Variables */
 
@@ -7,8 +8,6 @@ import random from "canvas-sketch-util/random"
 let canvasHeight
 let canvasWidth
 let context
-let startTime = null
-let currentTime = 0
 
 /* Grid */
 let gridData = []
@@ -23,46 +22,33 @@ const palette = [
 ]
 
 /* Cursor */
+const lineMvmtEaseFn = BezierEasing(0.76, 0.02, 0.73, 0.93)
+const lastLineMvmtEaseFn = BezierEasing(0.7, 0.01, 0, 0.49)
 const cursor = {
   x: 0,
   y: 0,
   radius: 30,
 }
 
-/* Animation Timing */
+/* Animation controls */
+let startTime = null
+let currentTime = 0
+let foundFood = false
 let newLineTime = 0 // The time that a new line was selected for the cursor to travel
 let lineTravelSpeed = 1 // The length of time in seconds to move from point A to point B
+let linesToTravel = 5
+let linesTraveled = 0
 
-const drawCursor = () => {
-  const [startX, startY] = gridData[cursor.previousPointIndex].point
-  const [endX, endY] = gridData[cursor.nextPointIndex].point
+const init = () => {
+  /* Canvas */
+  canvasHeight = 1000
+  canvasWidth = 1000
 
-  // Value from 0 -> 1 representing the percentage distance completed from startPoint to endPoint
-  const lineCompletePercentage = (currentTime - newLineTime) / lineTravelSpeed
-  // if (!foundFood) {
-  if (lineCompletePercentage <= 1) {
-    // Update x and y with custom easing function
-
-    cursor.x = lerp(startX, endX, lineCompletePercentage)
-    cursor.y = lerp(startY, endY, lineCompletePercentage)
-  }
-
-  context.beginPath()
-  context.fillStyle = "rgb(185,211,176)"
-  context.arc(cursor.x, cursor.y, cursor.radius, 0, Math.PI * 2)
-  context.fill()
-}
-
-const drawGrid = () => {
-  gridData.forEach((data, index) => {
-    const color = palette[index % palette.length]
-    context.fillStyle = color
-
-    const [x, y] = data.point
-    context.beginPath()
-    context.arc(x, y, gridOrbRadius, 0, Math.PI * 2)
-    context.fill()
-  })
+  /* Cursor */
+  const previousPointIndex = random.rangeFloor(0, gridSize * gridSize - 1)
+  const nextPointIndex = random.rangeFloor(0, gridSize * gridSize - 1)
+  cursor.previousPointIndex = previousPointIndex
+  cursor.nextPointIndex = nextPointIndex
 }
 
 const getGridData = (foodOptions, width, height) => {
@@ -92,16 +78,48 @@ const getGridData = (foodOptions, width, height) => {
   return options
 }
 
-const init = () => {
-  /* Canvas */
-  canvasHeight = 1000
-  canvasWidth = 1000
+const moveToNextPoint = () => {
+  cursor.previousPointIndex = cursor.nextPointIndex
+  cursor.nextPointIndex = random.rangeFloor(0, gridSize * gridSize - 1)
+  newLineTime = currentTime
+  linesTraveled++
+  if (linesTraveled >= linesToTravel && !foundFood) {
+    foundFood = true
+  }
+}
 
-  /* Cursor */
-  const previousPointIndex = random.rangeFloor(0, gridSize * gridSize - 1)
-  const nextPointIndex = random.rangeFloor(0, gridSize * gridSize - 1)
-  cursor.previousPointIndex = previousPointIndex
-  cursor.nextPointIndex = nextPointIndex
+/* Drawing functions */
+
+const drawCursor = () => {
+  const [startX, startY] = gridData[cursor.previousPointIndex].point
+  const [endX, endY] = gridData[cursor.nextPointIndex].point
+
+  const easeFn =
+    linesTraveled === linesToTravel - 1 ? lastLineMvmtEaseFn : lineMvmtEaseFn
+  const lineCompletePercentage = (currentTime - newLineTime) / lineTravelSpeed
+  if (lineCompletePercentage <= 1) {
+    cursor.x = lerp(startX, endX, easeFn(lineCompletePercentage))
+    cursor.y = lerp(startY, endY, easeFn(lineCompletePercentage))
+  } else {
+    moveToNextPoint()
+  }
+
+  context.beginPath()
+  context.fillStyle = "rgb(185,211,176)"
+  context.arc(cursor.x, cursor.y, cursor.radius, 0, Math.PI * 2)
+  context.fill()
+}
+
+const drawGrid = () => {
+  gridData.forEach((data, index) => {
+    const color = palette[index % palette.length]
+    context.fillStyle = color
+
+    const [x, y] = data.point
+    context.beginPath()
+    context.arc(x, y, gridOrbRadius, 0, Math.PI * 2)
+    context.fill()
+  })
 }
 
 const draw = time => {
@@ -116,7 +134,11 @@ const draw = time => {
   drawGrid()
   drawCursor()
 
-  window.requestAnimationFrame(draw)
+  if (!foundFood) {
+    window.requestAnimationFrame(draw)
+  } else {
+    console.log("Animation done")
+  }
 }
 
 // TODO Add draw function
