0ca8a4285c7cf6596b29aba12a68e8870f4817fb
Chris Queen <chrisqueen10@gmail.com>
2020-06-24 18:09:34 -0400

    Fetch image data after auth

--- src/pages/tree.js
+++ src/pages/tree.js
@@ -1,21 +1,38 @@
-import React from "react"
-
+import React, { useEffect, useState } from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const requestInstagramAuth = async () => {
+const requestInstagramAuth = () => {
   const authURL = `https://api.instagram.com/oauth/authorize?client_id=${process.env.GATSBY_INSTAGRAM_CLIENT_ID}&redirect_uri=${process.env.GATSBY_INSTAGRAM_REDIRECT_URI}&scope=user_profile,user_media&response_type=code`
   window.location = authURL
 }
 
-const TreePage = () => (
-  <Layout>
-    <SEO title="Tree" />
-    <h1>Tree page</h1>
-    <button onClick={requestInstagramAuth}>
-      I dare you to press this button
-    </button>
-  </Layout>
-)
+const TreePage = () => {
+  const [imageData, setImageData] = useState(null)
+  useEffect(async () => {
+    const searchParams = window.location.search
+    if (searchParams && searchParams.includes("code")) {
+      const code = searchParams.split("code=")[1]
+      // TODO Error handling
+      const res = await fetch(
+        `https://graph.instagram.com/me/media?fields=media_url,media_type,id,timestamp&access_token=${code}`
+      )
+      const imageData = await res.json()
+      console.log("Image Data: ", imageData)
+      setImageData(imageData)
+    }
+  })
+  return (
+    <Layout>
+      <SEO title="Tree" />
+      <h1>Tree page</h1>
+      {!imageData && (
+        <button onClick={requestInstagramAuth}>
+          I dare you to press this button
+        </button>
+      )}
+    </Layout>
+  )
+}
 
 export default TreePage
