17e746b3de109ed89eca5c5eb56f79a42904d6a0
Chris Queen <chrisqueen10@gmail.com>
2020-06-24 17:35:27 -0400

    Added requestInstagramAuth function

--- src/pages/tree.js
+++ src/pages/tree.js
@@ -3,10 +3,18 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+const requestInstagramAuth = async () => {
+  const authURL = `https://api.instagram.com/oauth/authorize?client_id=${process.env.GATSBY_INSTAGRAM_CLIENT_ID}&redirect_uri=${process.env.GATSBY_INSTAGRAM_REDIRECT_URI}&scope=user_profile,user_media&response_type=code`
+  window.location = authURL
+}
+
 const TreePage = () => (
   <Layout>
     <SEO title="Tree" />
     <h1>Tree page</h1>
+    <button onClick={requestInstagramAuth}>
+      I dare you to press this button
+    </button>
   </Layout>
 )
 
