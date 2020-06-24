3c3fb99edc78a2bf473633dafb8580e0004999e9
Chris Queen <chrisqueen10@gmail.com>
2020-06-24 16:10:40 -0400

    Added tree page

---
+++ src/pages/tree.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const TreePage = () => (
+  <Layout>
+    <SEO title="Tree" />
+    <h1>Tree page</h1>
+  </Layout>
+)
+
+export default TreePage
