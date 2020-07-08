d8911ce420e531b1e240cab6cd16dcbbb6da30e1
Chris Queen <chrisqueen10@gmail.com>
2020-07-08 10:09:19 -0400

    Added link to tree page on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,17 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Very much under construction</h1>
-    <Link to="/food/">Food Roulette</Link>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div>
+      <Link to="/food/">Food Roulette</Link>
+    </div>
+    <div>
+      <Link to="/tree/">dendrogram</Link>
     </div>
   </Layout>
 )
