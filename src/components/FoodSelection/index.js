61a5dd979b81d4e267d623bda1e8b1edbb323d52
Chris Queen <chrisqueen10@gmail.com>
2020-07-15 14:53:13 -0400

    Moved all food selection markup to FoodSelection component instead of pages/food file

---
+++ src/components/FoodSelection/index.js
@@ -0,0 +1,48 @@
+import React, { useLayoutEffect, useState, useEffect } from "react"
+import styled from "styled-components"
+import FoodAnimation from "../../lib/animation/food"
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+  height: 80vh;
+  width: 100%;
+`
+
+const Canvas = styled.canvas`
+  display: block;
+  margin: 0 auto;
+`
+
+const FoodSelection = ({ foodOptions }) => {
+  const [canvasSize, setCanvasSize] = useState(null)
+
+  /* Once the DOM loads, set the canvas dimensions to the min between container height and container width. */
+  useLayoutEffect(() => {
+    const wrapperElement = document.querySelector("#container")
+    const wrapperWidth = wrapperElement.offsetWidth
+    const wrapperHeight = wrapperElement.offsetHeight
+    setCanvasSize(Math.min(wrapperWidth, wrapperHeight))
+  }, [])
+
+  /* Once the canvas has proper dimensions, load the animation */
+  useEffect(() => {
+    if (canvasSize) {
+      const animation = new FoodAnimation(foodOptions)
+      animation.startSearchingAnimation()
+    }
+  }, [canvasSize])
+
+  return (
+    <Wrapper>
+      {!!canvasSize && (
+        <Canvas
+          width={canvasSize}
+          height={canvasSize}
+          id="#food-canvas"
+        ></Canvas>
+      )}
+    </Wrapper>
+  )
+}
+
+export default FoodSelection
--- src/pages/food.js
+++ src/pages/food.js
@@ -5,82 +5,13 @@ import { graphql } from "gatsby"
 // import { showCanvas, startAnimation } from "../lib/animation/food/index.js"
 import FoodAnimation from "../lib/animation/food/index.js"
 
+import FoodSelection from "../components/FoodSelection"
+
 const FoodRoulettePage = ({ data }) => {
-  const [selectedFoodOption, setSelectedFoodOption] = useState(null)
-  const [foodType, setFoodType] = useState("dinner")
-  const [canvasDimensions, setCanvasDimensions] = useState(0)
-
-  useLayoutEffect(() => {
-    const wrapperElement = document.getElementById("container")
-    const wrapperWidth = wrapperElement.offsetWidth
-    const wrapperHeight = wrapperElement.offsetHeight
-
-    const newCanvasDimensions =
-      wrapperWidth > wrapperHeight ? wrapperHeight : wrapperWidth
-    console.log("Dimension: ", newCanvasDimensions)
-    setCanvasDimensions(newCanvasDimensions)
-  }, [])
-
-  useEffect(() => {
-    if (canvasDimensions > 0) {
-      // showCanvas(data["dinner"].edges)
-    }
-  }, [canvasDimensions])
-
-  useEffect(() => {
-    console.log("Food option: ", selectedFoodOption)
-    if (document) {
-      const canvas = document.getElementById("food-canvas")
-
-      if (canvas) {
-        canvas.style.opacity = "0"
-      }
-    }
-  }, [selectedFoodOption])
-
-  console.log("selectedFoodOption: ", selectedFoodOption)
-  // TODO Move markup to components in the components folder
   return (
     <Layout>
-      <SEO title="Food Roulette" />
-
-      <button>Start Idle</button>
-
-      <button
-        onClick={() => {
-          const animation = new FoodAnimation(
-            data["dinner"].edges,
-            setSelectedFoodOption
-          )
-          animation.startSearchingAnimation()
-        }}
-      >
-        Start Selection
-      </button>
-
-      <div
-        id="canvas-wrapper"
-        style={{ height: "90vh", width: "100%", position: "relative" }}
-      >
-        {canvasDimensions > 0 && (
-          <canvas
-            width={canvasDimensions}
-            height={canvasDimensions}
-            id="#food-canvas"
-            style={{
-              display: "block",
-              margin: "0 auto",
-              opacity: `${selectedFoodOption ? "0" : "1"}`,
-              transition: `3s ease 0s opacity`,
-            }}
-          ></canvas>
-        )}
-      </div>
-      {selectedFoodOption && (
-        <div style={{ margin: "0 auto" }}>
-          <h1>{selectedFoodOption.foodOptionData.name}</h1>
-        </div>
-      )}
+      <SEO title="Food Selection," />
+      <FoodSelection foodOptions={data["dinner"].edges} />
     </Layout>
   )
 }
