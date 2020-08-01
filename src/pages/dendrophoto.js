bb67edeceacf23d38947993a99a87e10c3c47d1c
Chris Queen <chrisqueen10@gmail.com>
2020-08-01 16:36:17 -0400

    Created 'DendroPhoto' page

---
+++ src/pages/dendrophoto.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const DendroPhotoPage = () => (
+  <Layout>
+    <SEO title="DendroPhoto" />
+    <h1>Dendro-Photo</h1>
+  </Layout>
+)
+
+export default DendroPhotoPage
